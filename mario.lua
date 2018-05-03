874756ab95497c38d7b03989b1eae7379255538e
lulbasaur <lukas.hamberg@gmail.com>
2018-05-03 14:49:28 +0200

    init commit

---
+++ mario.lua
@@ -0,0 +1,23 @@
+if gameinfo.getromname() == "Super Mario World (USA)" then
+	Filename = "DP1.state"
+	ButtonNames = {
+		"A",
+		"B",
+		"X",
+		"Y",
+		"Up",
+		"Down",
+		"Left",
+		"Right",
+	}
+elseif gameinfo.getromname() == "Super Mario Bros." then
+	Filename = "SMB1-1.state"
+	ButtonNames = {
+		"A",
+		"B",
+		"Up",
+		"Down",
+		"Left",
+		"Right",
+	}
+end
