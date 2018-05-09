fe11ce92794c860c72fe863436947acc6bd5bd3a
lulbasaur <lukas.hamberg@gmail.com>
2018-05-09 19:06:09 +0200

    lets e go

---
+++ mario-netknights.lua
@@ -0,0 +1,31 @@
+-- set buttons and file name
+if gameinfo.getromname() == "Super Mario World (USA)" then
+  Filename = "DP1.state"
+  ButtonNames = {
+    "A",
+    "B",
+    "Up",
+    "Down",
+    "Left",
+    "Right",
+  }
+end
+
+function getPositions()
+	if gameinfo.getromname() == "Super Mario World (USA)" then
+		marioX = memory.read_s16_le(0x94)
+		marioY = memory.read_s16_le(0x96)
+
+		local layer1x = memory.read_s16_le(0x1A);
+		local layer1y = memory.read_s16_le(0x1C);
+
+		screenX = marioX-layer1x
+		screenY = marioY-layer1y
+  end
+end
+
+
+while true do
+
+  emu.frameadvance()
+end
