d63dbe79bf25a86af2001c90554f4e4cac65423c
pohi8056 <philding@hotmail.com>
2018-05-09 10:29:17 -0700

    /

--- mario-netknights.lua
+++ mario-netknights.lua
@@ -17,6 +17,73 @@ function getPositions()
 	screenY = marioY-layer1y
 end
 
+function getTile(dx, dy)
+	x = math.floor((marioX+dx+8)/16)
+	y = math.floor((marioY+dy)/16)
+
+	return memory.readbyte(0x1C800 + math.floor(x/0x10)*0x1B0 + y*0x10 + x%0x10)
+end
+
+function getSprites()
+	local sprites = {}
+	for slot=0,11 do
+		local status = memory.readbyte(0x14C8+slot)
+		if status ~= 0 then
+			spritex = memory.readbyte(0xE4+slot) + memory.readbyte(0x14E0+slot)*256
+			spritey = memory.readbyte(0xD8+slot) + memory.readbyte(0x14D4+slot)*256
+			sprites[#sprites+1] = {["x"]=spritex, ["y"]=spritey}
+		end
+	end
+	return sprites
+end
+
+function getExtendedSprites()
+	local extended = {}
+	for slot=0,11 do
+		local number = memory.readbyte(0x170B+slot)
+		if number ~= 0 then
+			spritex = memory.readbyte(0x171F+slot) + memory.readbyte(0x1733+slot)*256
+			spritey = memory.readbyte(0x1715+slot) + memory.readbyte(0x1729+slot)*256
+			extended[#extended+1] = {["x"]=spritex, ["y"]=spritey}
+		end
+	end
+	return extended
+end
+
+function getInputs()
+	getPositions()
+
+	sprites = getSprites()
+	extended = getExtendedSprites()
+
+	local inputs = {}
+
+	for dy=-BoxRadius*16,BoxRadius*16,16 do
+		for dx=-BoxRadius*16,BoxRadius*16,16 do
+			inputs[#inputs+1] = 0
+
+			tile = getTile(dx, dy)
+			if tile == 1 and marioY+dy < 0x1B0 then
+				inputs[#inputs] = 1
+			end
+
+			for i = 1,#sprites do
+				distx = math.abs(sprites[i]["x"] - (marioX+dx))
+				disty = math.abs(sprites[i]["y"] - (marioY+dy))
+				if distx <= 8 and disty <= 8 then
+					inputs[#inputs] = -1
+				end
+			end
+
+			for i = 1,#extended do
+				distx = math.abs(extended[i]["x"] - (marioX+dx))
+				disty = math.abs(extended[i]["y"] - (marioY+dy))
+				if distx < 8 and disty < 8 then
+					inputs[#inputs] = -1
+				end
+			end
+		end
+	end
 
 while true do
 
